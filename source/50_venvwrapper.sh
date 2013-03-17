6085bb4217e69f3726debe99b67aa6813724618f
Martijn van Exel <m@rtijn.org>
2013-03-17 15:16:13 -0600

    added include for virtualenvwrapper

---
+++ source/50_venvwrapper.sh
@@ -0,0 +1,6 @@
+# source the init script for virtualenvwrapper
+# http://virtualenvwrapper.readthedocs.org/en/latest/
+
+if [[ -e /usr/local/bin/virtualenvwrapper.sh ]]; then
+  source /usr/local/bin/virtualenvwrapper.sh
+fi
