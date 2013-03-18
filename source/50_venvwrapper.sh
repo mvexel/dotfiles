cb40e0a7d68d4cb597ab4c63a0f1bbd78932b910
Martijn van Exel <m@rtijn.org>
2013-03-17 19:59:47 -0600

    added envvar for virtualenvwrapper

--- source/50_venvwrapper.sh
+++ source/50_venvwrapper.sh
@@ -2,5 +2,7 @@
 # http://virtualenvwrapper.readthedocs.org/en/latest/
 
 if [[ -e /usr/local/bin/virtualenvwrapper.sh ]]; then
+  export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+  export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
   source /usr/local/bin/virtualenvwrapper.sh
 fi
