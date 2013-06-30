4d3ddebc5022fe81fb130b12f7b1b68e63767d15
Martijn van Exel <m@rtijn.org>
2013-06-30 11:36:01 -0600

    cleaned up venvwrapper scheisse

--- source/50_venvwrapper.sh
+++ source/50_venvwrapper.sh
@@ -1,14 +1,14 @@
 PATH=/usr/local/share/python:${PATH}
 
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2.7
+export VIRTUALENVWRAPPER_PYTHON=`which python`
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
 if [[ -r /usr/local/share/python/virtualenvwrapper.sh ]]; then
     source /usr/local/share/python/virtualenvwrapper.sh
 elif [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
-    source /usr/local/share/python/virtualenvwrapper.sh
+    source /usr/local/bin/virtualenvwrapper.sh
 else
     echo "WARNING: Can't find virtualenvwrapper.sh"
 fi
